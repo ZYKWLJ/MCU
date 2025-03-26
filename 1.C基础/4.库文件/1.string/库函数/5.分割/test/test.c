31a7c32e90f1460b00710b1a05049c65f0026e4a
zyk <2126483838@qq.com>
2025-03-26 23:46:06 +0800

    printf打印函数的本质,遇到\0则停止

--- 1.C基础/4.库文件/1.string/库函数/5.分割/test/test.c
+++ 1.C基础/4.库文件/1.string/库函数/5.分割/test/test.c
@@ -2,19 +2,18 @@
 #include <string.h>
 int main()
 {
-    char chars[100] = "123,dsg,,43,;';',-=][],qwe";
+    char chars[100] = "page       1      ";
     // 这是标准使用！先token，再NULL！
-    // str：要进行分割的字符串。首次调用 strtok 时，需要传入待分割的字符串；
-    // 后续调用时，该参数应设为 NULL，以便函数继续从上次分割的位置继续处理。
-    // delim：一个`包含分隔符`的字符串，strtok 会根据这些分隔符来分割字符串。
-
-    char *ptr = strtok(chars, "/,n");//返回当前的指针,一旦找到分隔符，就把它替换成字符串结束符 '\0'。
+    char *ptr = strtok(chars, " ");//返回当前的指针,一旦找到分隔符，就把它替换成字符串结束符 '\0'。
     while (ptr)
     {
         printf("%s\n", ptr);
-        ptr= strtok(NULL, "/,n");//设置NULL，以便于从上次分割的位置继续处理！
+        ptr= strtok(NULL, " ");//设置NULL，以便于从上次分割的位置继续处理！
     }
-    printf("\n\nchars==%s\n\n",chars);//输出123.原字符串已被更改
+    printf("\n\nchars==%s\n\n",chars);//输出page.原字符串已被更改
 
+    char str[10]="123456789";
+    str[3]='\0';
+    printf("str==%s\n\n",str);//这个事实证明printf打印的函数的截止是'\0'！
     return 0;
 }
