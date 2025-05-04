ecd736a97c54938ecdb0661217a7d7da60089c5b
zyk <2126483838@qq.com>
2025-05-04 11:35:27 +0800

    共用体的使用,同一时刻为一个

--- 1.C基础/2.高级数据类型/4.共用体/test/test.c
+++ 1.C基础/2.高级数据类型/4.共用体/test/test.c
@@ -6,8 +6,7 @@ typedef union
     int i;
     char c[25];
     // double d;/*最大对齐数是8*/
-
-    int d;/*最大对齐数是4*/
+    double d; /*最大对齐数是4*/
 } UNION;
 
 int main()
@@ -17,17 +16,31 @@ int main()
     // 初始化成员 c
     strcpy(u.c, "hello");
     printf("u.c=%s\t", u.c);
-    printf("Total Size=%zu\n",sizeof(u));
+    printf("u.i=%d\t", u.i);
+    printf("u.d=%lf\t", u.d);
+    printf("Total Size=%zu\n", sizeof(u));
 
     // 初始化成员 i
     u.i = 123;
-    printf("u.i=%d\t\t", u.i);
-    printf("Total Size=%zu\n",sizeof(u));
+    printf("u.c=%s\t", u.c);
+    printf("u.i=%d\t", u.i);
+    printf("u.d=%lf\t", u.d);
+    printf("Total Size=%zu\n", sizeof(u));
 
     // 初始化成员 d
     u.d = 3.14;
-    printf("u.d=%f\t", u.d);
-    printf("Total Size=%zu\n",sizeof(u));
+    printf("u.c=%s\t", u.c);
+    printf("u.i=%d\t", u.i);
+    printf("u.d=%lf\t", u.d);
+    printf("Total Size=%zu\n", sizeof(u));
 
     return 0;
 }
+
+/*
+以下结果充分证明了共用体的特点：
+同一时间只能初始化一个成员，其他的会被覆盖！乱码！
+$ /home/eyk/桌面/MCU/1.C基础/2.高级数据类型/4.共用体/test/test
+u.c=hello       u.i=1819043176  u.d=0.000000    Total Size=32
+u.c={   u.i=123 u.d=0.000000    Total Size=32
+u.c=��Q�        @       u.i=1374389535  u.d=3.140000    Total Size=32*/
--- 1.C基础/4.库文件/1.string/0.test/test.c
+++ 1.C基础/4.库文件/1.string/0.test/test.c
@@ -1,19 +1,75 @@
 #include <stdio.h>
 #include <string.h>
-
-int main() {
-    const char* str = "Hello, World!";
-    int c = 'o';
-    size_t n = strlen(str);
-
-    // 使用 memchr 函数查找字符 'o'
-    char *result = (char *)memchr(str, c, n);
-
-    if (result != NULL) {
-        printf("字符 '%c' 首次出现在位置 %ld。\n", c, result - str);
-    } else {
-        printf("未找到字符 '%c'。\n", c);
+// #define MEMCPY
+// #define MEMMOVE
+// #define MEMSET_MEMMOVE
+#define MEMSET
+int main()
+{
+    char *str = "Hello, World!";
+#ifdef MEMCPY
+    // 目的地
+    char dest[20];
+    memcpy(dest, str + 1, 20);
+    // 在目的地的末尾添加一个空字符
+    dest[20] = '\0';
+    printf("Copied string: %s\n", dest);
+    for (int i = 0; i < 20; i++)
+    {
+        printf("%c--%d\n", dest[i], i);
     }
+    dest[4] = '\0';
+    /**
+     * data descp: printf会一直打印直到遇到\0为止，所以我们需要在目的地的末尾添加一个空字符，否则会一直打印到内存的末尾。
+     */
+    printf("%s\n", dest);
+#endif
 
+#ifdef MEMMOVE
+    char dest[20];
+    memmove(dest, str + 1, 20);
+    // 在目的地的末尾添加一个空字符
+    dest[20] = '\0';
+    printf("Copied string: %s\n", dest);
+    for (int i = 0; i < 20; i++)
+    {
+        printf("%c--%d\n", dest[i], i);
+    }
+    dest[4] = '\0';
+    printf("%s\n", dest);
+#endif
+
+#ifdef MEMSET_MEMMOVE
+    {
+        char dest[20] = "1234";
+        memcpy(dest + 2, dest, strlen(dest));
+        // 在目的地的末尾添加一个空字符
+        dest[strlen(dest)] = '\0';
+        printf("Copied string: %s\n", dest);
+    }
+    {
+        char dest[] = "1234";
+        memmove(dest + 2, dest, strlen(dest));
+        // 在目的地的末尾添加一个空字符
+        dest[strlen(dest)] = '\0';
+        printf("Copied string: %s\n", dest);
+    }
+    {
+        char str[] = "GNU/Linux";
+        memmove(str + 4, str, 5); // 将 "GNU/" 移动到 "Linux" 前
+        printf("%s\n", str);      // 输出: GNUGNU/L
+    }
+#endif
+
+#ifdef MEMSET
+    {
+        char dest[20] = "1234";
+        memset(dest + 4, 'A', 20);
+        // 在目的地的末尾添加一个空字符
+        dest[19] = '\0';
+        printf("Copied string: %s\n", dest);
+        printf("Copied string: %s\n", memset(dest + 4, 'A', 20));
+    }
+#endif
     return 0;
-}    
+}
