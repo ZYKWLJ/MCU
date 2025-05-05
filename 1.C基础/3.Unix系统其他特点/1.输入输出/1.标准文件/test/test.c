60a5619857374d2dcd191038c5840b398982bbe3
zyk <2126483838@qq.com>
2025-05-05 18:02:32 +0800

    C语言中各大IO函数,共16个

--- 1.C基础/3.Unix系统其他特点/1.输入输出/1.标准文件/test/test.c
+++ 1.C基础/3.Unix系统其他特点/1.输入输出/1.标准文件/test/test.c
@@ -1,5 +1,6 @@
 # include<stdio.h>
 int main(){
-
+fread();
+fwrite();
 return 0;
 }
--- 1.C基础/6.经典100例/1-10/1.无重复数字的三位数.c
+++ 1.C基础/6.经典100例/1-10/1.无重复数字的三位数.c
@@ -7,38 +7,44 @@
 * create time:2025/03/29 21:02:20
 
 */
-# include<stdio.h>
+#include <stdio.h>
 
-void dfs(int startIndex,int *nums,int numsSize,int *used,int *path,int pathTop,int ans[][3],int ansTop){
-    if(pathTop==3){
-        for(int i=0;i<3;i++){
-            printf("%d",path[i]);
+void dfs(int startIndex, int *nums, int numsSize, int *used, int *path, int pathTop, int ans[][3], int ansTop)
+{
+    if (pathTop == 3)
+    {
+        for (int i = 0; i < 3; i++)
+        {
+            printf("%d", path[i]);
         }
         printf("\n");
         return;
-        for(int i=0;i<3;i++){
-            ans[ansTop][i]=path[i];
+        for (int i = 0; i < 3; i++)
+        {
+            ans[ansTop][i] = path[i];
         }
         ansTop++;
         return;
     }
-    for(int i=0;i<5;i++){//遍历nums数组！
-        if(used[i]==1)
+    for (int i = 0; i < 5; i++)
+    { // 遍历nums数组！
+        if (used[i] == 1)
             continue;
-        used[i]=1;
-        path[pathTop++]=nums[i];//这里模拟栈的核心！使用pathTop来模拟栈顶的指针！
-        dfs(0,nums,numsSize,used,path,pathTop,ans,ansTop);
+        used[i] = 1;
+        path[pathTop++] = nums[i]; // 这里模拟栈的核心！使用pathTop来模拟栈顶的指针！
+        dfs(0, nums, numsSize, used, path, pathTop, ans, ansTop);
         pathTop--;
-        used[i]=0;
+        used[i] = 0;
     }
 }
-int main(){
-    int ans[][3]={0};
-    int used[4]={0};
-    int path[3]={0};
-    int pathTop=0;
-    int nums[]={1,2,3,4};
-    int ansTop=0;
-    dfs(0,nums,4,used,path,pathTop,ans,ansTop);
+int main()
+{
+    int ans[][3] = {0};
+    int used[4] = {0};
+    int path[3] = {0};
+    int pathTop = 0;
+    int nums[] = {1, 2, 3, 4};
+    int ansTop = 0;
+    dfs(0, nums, 4, used, path, pathTop, ans, ansTop);
     return 0;
 }
